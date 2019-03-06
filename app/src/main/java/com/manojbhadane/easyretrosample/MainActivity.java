13c8124d30445ad7635dfeab8a39d4c289938077
manoj.bhadane <manojbhadane777@gmail.com>
2019-03-06 17:53:48 +0530

    minor changes

--- app/src/main/java/com/manojbhadane/easyretrosample/MainActivity.java
+++ app/src/main/java/com/manojbhadane/easyretrosample/MainActivity.java
@@ -34,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
      * 3. Easy to Use
      * 4. Minimal configuration
      *
+     *
+     *  * Version Commit guide on Jitpack , execute follwing commands
+     *  * <p>
+     *  * git tag -a v1.0 -m "first commit"
+     *  * git push origin v1.2
+     *  *
+     *
      */
 
     private void callAPI() {
