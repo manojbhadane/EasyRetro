310f26c3303afa14d53d45141af1e8f3059d53b0
manoj.bhadane <manojbhadane777@gmail.com>
2019-06-10 11:33:53 +0530

    Rxjava support added

--- easyretro/src/main/java/com/manojbhadane/easyretro/RetrofitInstance.java
+++ easyretro/src/main/java/com/manojbhadane/easyretro/RetrofitInstance.java
@@ -3,6 +3,7 @@ package com.manojbhadane.easyretro;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
@@ -25,6 +26,7 @@ public class RetrofitInstance {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .client(client)
                     .build();
         }
